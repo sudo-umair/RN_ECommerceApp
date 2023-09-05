6125e9f0c5552ad3b7070a9eba6696e1d2772003
Muhammad Umair <muhammadumair523@gmail.com>
2023-09-05 14:11:18 +0500

    path aliases setup

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,19 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          assets: './src/assets',
+          api: './src/api',
+          common: './src/common',
+          components: './src/components',
+          interfaces: './src/interfaces',
+          navigation: './src/navigation',
+          screens: './src/screens',
+        },
+      },
+    ],
+  ],
 };
